086a1d646d938bbd6814d77b72161bf055439653
derian95 <dfhamezquita@gmail.com>
2023-05-11 16:50:10 -0500

    ra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import video2 from './assets/video.mp4'
+import iamgenCasino from './assets/casino1.jpg'
 import { Cursor } from './components/ui/Cursor'
 import { SideBar } from './components/ui/SideBar'
 function App() {
@@ -9,7 +10,7 @@ function App() {
 		<div style={{width:'100vw'}}>
 			<Cursor/>
 			<SideBar  />
-			<div className='video-background'>
+			<div className='video-background' id='inicio'>
 				<video autoPlay loop muted>
 					<source src={video2} type='video/mp4' />
 				</video>
@@ -23,9 +24,37 @@ function App() {
 					<p>Casino de casino de los casinos flo flo flo</p>
 				</div> */}
 			</div>
-			<div style={{ height:'120vh', backgroundColor:'#191919',padding:'25px 50px'}}>
-				<h4 className='test'>SOBRE NOSOTROS</h4>
-				<p style={{ maxWidth:'700px'}} className='text'>Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+			<div style={{  backgroundColor:'#191919',padding:'25px 50px'}}>
+				<div style={{paddingRight:100, marginBottom:'80px'}} id='nosotros'>
+					<h4 className='test'>SOBRE NOSOTROS</h4>
+					<p  className='text'>Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+				
+				</div>
+
+				<div style={{paddingRight:100, display:'flex', flexWrap:'wrap', marginBottom:80}}>
+					<div style={{width:'48%', height:'50vh', backgroundColor:'orange'}}> <img src={iamgenCasino} alt="" width={'100%'} height={'100%'}/> </div>
+					<div style={{width:'48%', height:'50vh', backgroundColor:'rgb(13 15 20)',color:'white'}}>ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</div>
+					
+					<div style={{width:'48%', height:'50vh', backgroundColor:'rgb(13 15 20)',color:'white'}}>ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</div>
+					<div style={{width:'48%', height:'50vh', backgroundColor:'blue'}}><img src={iamgenCasino} alt="" width={'100%'} height={'100%'}/></div>
+				</div>
+				<div style={{paddingRight:100, marginBottom:'80px',width:'50vh'}} id='section1' >
+					<h4 className='test'>METODOS DE PAGO</h4>
+					<p  className='text'>Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+				
+				</div>
+
+				<div  style={{paddingRight:100, marginBottom:'80px',width:'50vh'}} id='juegos'>
+					<h4 className='test'>JUEGOS</h4>
+					<p  className='text'>Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+				
+				</div>
+
+				<div style={{paddingRight:100, marginBottom:'80px',width:'50vh'}} id='contacto'>
+					<h4 className='test'>JUEGOS</h4>
+					<p  className='text'>Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan. Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana. Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+				
+				</div>
 			</div>
 		</div>
 	)
--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,45 +1,92 @@
-import {  useState } from 'react'
+import { useState } from 'react'
 import '../SideBar.css'
 import logoExcalibur from '../../assets/excalibur.png'
 import nombreSala from '../../assets/excalibur.svg'
-
+import { Link } from 'react-scroll'
 
 export const SideBar = () => {
-
-    const [showMenu, setShowMenu] = useState(false)
+	const [showMenu, setShowMenu] = useState(false)
 
 	const toggleMenu = () => {
 		setShowMenu((prev) => !prev)
 	}
 
-  return (
-    <aside className={ `aside ${ showMenu ? 'show' : 'hide' }` }>
-        <div className="aside-contain">
-            <img src={logoExcalibur} alt="logo" style={{width:34}}/>
-            <img src={nombreSala} alt="logo" style={{height:110}}/>
-            <div className='contain-btn'>
-                <div className='button-open-menu' onClick={toggleMenu}>
-                        <p className='icon-button'>+</p>
-                </div>
-            </div>
-           
-        </div>
-        <div className='menu'>
-           
-           <div >
-           <p style={{color:'white', fontSize:25, fontWeight:'bold', textAlign:'center', marginBottom:15}}>EXPLORE</p>
-            <div>
-                <ol>
-                    <ul className='option'>Inicio</ul>
-                    <ul className='option'>Sobre nosotros</ul>
-                    <ul className='option'>Jueogs</ul>
-                    <ul className='option'>Contacto</ul>
-                </ol>
-            </div>
-           </div>
-           
-            <p style={{color:'white'}}>Un casino es una instalación que alberga y tiene capacidad para ciertos juegos de azar. Los casinos son comúnmente construidos cerca o en combinación con hoteles, restaurantes, tiendas comerciales, cruceros turísticos y otras atracciones.</p>
-        </div>
-    </aside>
-  )
+	return (
+		<aside className={`aside ${showMenu ? 'show' : 'hide'}`}>
+			<div className='aside-contain'>
+				<img src={logoExcalibur} alt='logo' style={{ width: 34 }} />
+				<img src={nombreSala} alt='logo tr' style={{ height: 110 }} />
+				<div className='contain-btn'>
+					<div className='button-open-menu' onClick={toggleMenu}>
+						<p className='icon-button'>+</p>
+					</div>
+				</div>
+			</div>
+			<div className='menu'>
+				<div>
+					<p
+						style={{
+							color: 'white',
+							fontSize: 25,
+							fontWeight: 'bold',
+							textAlign: 'center',
+							marginBottom: 15,
+						}}>
+						EXPLORE
+					</p>
+					<div>
+						<ol>
+							<ul className='option'>
+								<Link
+									activeClass='active'
+									to='inicio'
+									spy={true}
+									smooth={true}
+									offset={-70}
+									duration={500}>
+									INICIO
+								</Link>
+							</ul>
+							<ul className='option'><Link
+									activeClass='active'
+									to='nosotros'
+									spy={true}
+									smooth={true}
+									offset={-70}
+									duration={500}>
+									SOBRE NOSOTROS
+								</Link></ul>
+							<ul className='option'>
+                            <Link
+									activeClass='active'
+									to='juegos'
+									spy={true}
+									smooth={true}
+									offset={-70}
+									duration={500}>
+									JUEGOS
+								</Link>
+                            </ul>
+							<ul className='option'><Link
+									activeClass='active'
+									to='contacto'
+									spy={true}
+									smooth={true}
+									offset={-70}
+									duration={500}>
+									CONTACTO
+								</Link></ul>
+						</ol>
+					</div>
+				</div>
+
+				<p style={{ color: 'white' }}>
+					Un casino es una instalación que alberga y tiene capacidad para
+					ciertos juegos de azar. Los casinos son comúnmente construidos cerca o
+					en combinación con hoteles, restaurantes, tiendas comerciales,
+					cruceros turísticos y otras atracciones.
+				</p>
+			</div>
+		</aside>
+	)
 }
