f51418e7eea93d7d724d0f5d73235434b5b77e86
derian95 <dfhamezquita@gmail.com>
2023-05-23 23:07:04 -0500

    test

--- src/pages/NewAbout.tsx
+++ src/pages/NewAbout.tsx
@@ -1,19 +1,32 @@
 import { motion } from 'framer-motion'
 
 export const NewAbout = () => {
+
 	
-
 	return (
-		<div className='flex z-10' id='about' >
-			<div className='h-screen min-w-[100vw] flex items-center justify-center gap-14 '>
-				<motion.div 
-                  initial={{opacity:0, translateX:200}}
-                  whileInView={{opacity:1, translateX:0}}
-                  viewport={{once:true}}
-                  transition={{duration:2, ease:'backInOut'}}
-                >
-					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>NOSOTROS</p>
-					<p className='text-[#DDDDDD] font-normal max-w-md text-[0.875rem] selection:bg-orange-700' >
+		<div className='flex z-10' id='about'>
+			<div className='h-screen min-w-[150vw] flex items-center justify-center gap-2 '>
+			<div className='sticky left-0  w-80 h-screen -z-10 bg-red-700'>
+					<motion.img
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						viewport={{ once: true }}
+						transition={{ duration: 3, ease: 'backInOut' }}
+						className='w-full h-screen object-cover '
+						src='https://img.freepik.com/vector-gratis/ilustracion-realista-juegos-casino_52683-99917.jpg?w=2000'
+						alt=''
+					/>
+				</div>
+				<motion.div
+					initial={{ opacity: 0, translateX: 200 }}
+					whileInView={{ opacity: 1, translateX: 0 }}
+					viewport={{ once: true }}
+					transition={{ duration: 2, ease: 'backInOut' }}
+					>
+					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>
+						NOSOTROS
+					</p>
+					<p className='text-[#DDDDDD] font-normal max-w-md text-[0.875rem] selection:bg-orange-700'>
 						{' '}
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
 						officiis itaque expedita nulla corrupti autem. Asperiores cumque
@@ -21,25 +34,14 @@ export const NewAbout = () => {
 						quis illo. Reiciendis, quaerat! Quo.
 					</p>
 				</motion.div>
-
-				<motion.img
-                    initial={{opacity:0}}
-                    whileInView={{opacity:1}}
-                    viewport={{once:true}}
-                    transition={{duration:3, ease:'backInOut'}}
-					className='w-80 h-screen object-cover'
-					src='https://img.freepik.com/vector-gratis/ilustracion-realista-juegos-casino_52683-99917.jpg?w=2000'
-					alt=''
-				/>
-			</div>
-			<div className='h-screen min-w-[100vw] flex items-center justify-center gap-14 '>
-            <motion.div 
-                  initial={{opacity:0, translateX:200}}
-                  whileInView={{opacity:1, translateX:0}}
-                  viewport={{once:true}}
-                  transition={{duration:2, ease:'backInOut'}}
-                >
-					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>NOSOTROS</p>
+				<motion.div
+					initial={{ opacity: 0, translateX: 200 }}
+					whileInView={{ opacity: 1, translateX: 0 }}
+					viewport={{ once: true }}
+					transition={{ duration: 2, ease: 'backInOut' }}>
+					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>
+						NOSOTROS
+					</p>
 					<p className='text-[#DDDDDD] font-normal max-w-md text-[0.875rem] '>
 						{' '}
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
@@ -48,25 +50,14 @@ export const NewAbout = () => {
 						quis illo. Reiciendis, quaerat! Quo.
 					</p>
 				</motion.div>
-
-				<motion.img
-                    initial={{opacity:0}}
-                    whileInView={{opacity:1}}
-                    viewport={{once:true}}
-                    transition={{duration:3, ease:'backInOut'}}
-					className='w-80 h-screen object-cover'
-					src='https://img.freepik.com/vector-gratis/ilustracion-realista-juegos-casino_52683-99917.jpg?w=2000'
-					alt=''
-				/>
-			</div>
-			<div className='h-screen min-w-[100vw] flex items-center justify-center gap-14 '>
-            <motion.div 
-                  initial={{opacity:0, translateX:200}}
-                  whileInView={{opacity:1, translateX:0}}
-                  viewport={{once:true}}
-                  transition={{duration:2, ease:'backInOut'}}
-                >
-					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>NOSOTROS</p>
+				<motion.div
+					initial={{ opacity: 0, translateX: 200 }}
+					whileInView={{ opacity: 1, translateX: 0 }}
+					viewport={{ once: true }}
+					transition={{ duration: 2, ease: 'backInOut' }}>
+					<p className='text-5xl text-white font-bold  text-[2.625rem ]  mb-10'>
+						NOSOTROS
+					</p>
 					<p className='text-[#DDDDDD] font-normal max-w-md text-[0.875rem] '>
 						{' '}
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
@@ -75,17 +66,8 @@ export const NewAbout = () => {
 						quis illo. Reiciendis, quaerat! Quo.
 					</p>
 				</motion.div>
-
-				<motion.img
-                    initial={{opacity:0}}
-                    whileInView={{opacity:1}}
-                    viewport={{once:true}}
-                    transition={{duration:3, ease:'backInOut'}}
-					className='w-80 h-screen object-cover'
-					src='https://img.freepik.com/vector-gratis/ilustracion-realista-juegos-casino_52683-99917.jpg?w=2000'
-					alt=''
-				/>
 			</div>
+			
 		</div>
 	)
 }
--- src/pages/NewEvents.tsx
+++ src/pages/NewEvents.tsx
@@ -1,8 +1,20 @@
+import { motion } from "framer-motion"
 import { Card } from "../components/pages/anunciament/Card"
 
 export const NewEvents = () => {
   return (
-    <div className="h-screen w-auto flex justify-start  items-center gap-8  flex-nowrap" id="events">
+    <div className="h-screen w-auto flex justify-start  items-center gap-8  flex-nowrap relative" id="events">
+      <div className='sticky left-0 flex w-80 h-screen '>
+					<motion.img
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						viewport={{ once: true }}
+						transition={{ duration: 3, ease: 'backInOut' }}
+						className='w-80 h-screen object-cover '
+						src='https://img.freepik.com/vector-gratis/ilustracion-realista-juegos-casino_52683-99917.jpg?w=2000'
+						alt=''
+					/>
+				</div>
        <Card/>
         <Card/>
         <Card/>
